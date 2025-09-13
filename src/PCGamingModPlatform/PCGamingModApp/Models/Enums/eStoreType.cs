69f5ead70d796c08423fd52e7a0ca8917243eb9a
ff2009 <franciscoofernandes2008@hotmail.com>
2025-09-14 00:45:24 +0100

    #3 Created the GameItem class model.

---
+++ src/PCGamingModPlatform/PCGamingModApp/Models/Enums/eStoreType.cs
@@ -0,0 +1,15 @@
+﻿namespace PCGamingModApp.Models.Enums;
+
+// ---------------------------------------------------------------
+// Store enum – tells us which store badge to show
+// ---------------------------------------------------------------
+public enum eStoreType
+{
+    Unknown,
+    EaApp,
+    Epic,
+    GoG,
+    Steam,
+    Ubisoft,
+    Other // CD/DVD, manual installs, etc.
+}
