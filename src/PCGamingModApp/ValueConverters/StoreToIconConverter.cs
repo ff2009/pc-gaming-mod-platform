d49977a474bb1638aebceef7ae5c99b967e17cf5
ff2009 <franciscoofernandes2008@hotmail.com>
2025-09-28 12:24:26 +0100

    Refactor StoreToIconConverter to use updated StoreType enum.

--- src/PCGamingModApp/ValueConverters/StoreToIconConverter.cs
+++ src/PCGamingModApp/ValueConverters/StoreToIconConverter.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using Avalonia.Data.Converters;
 using PCGamingModApp.Core.Helpers;
-using PCGamingModApp.Core.Models.Enums;
+using PCGamingModApp.Data.Enums;
 
 namespace PCGamingModApp.ValueConverters;
 
@@ -10,15 +10,15 @@ public sealed class StoreToIconConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is eStoreType store)
+        if (value is StoreType store)
         {
             return store switch
             {
-                eStoreType.EaApp => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/ea.png")),
-                eStoreType.Epic => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/epic.png")),
-                eStoreType.GoG => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/gog.png")),
-                eStoreType.Steam => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/steam.png")),
-                eStoreType.Ubisoft => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/ubisoft.png")),
+                StoreType.EaApp => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/ea.png")),
+                StoreType.Epic => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/epic.png")),
+                StoreType.GoG => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/gog.png")),
+                StoreType.Steam => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/steam.png")),
+                StoreType.Ubisoft => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/ubisoft.png")),
                 _ => ImageHelper.LoadFromResource(new Uri("avares://PCGamingModApp/Assets/Images/StoreIcons/cd.png"))
             };
         }
