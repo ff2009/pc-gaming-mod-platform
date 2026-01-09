d2e6935dc97009b7a1733f3161f57465e7f55ed0
ff2009 <franciscoofernandes2008@hotmail.com>
2026-01-09 00:09:48 +0000

    Update ConvertBytesToUnit method to return long and support additional size units

--- src/PCGamingModApp.Core/Helpers/ConversionHelper.cs
+++ src/PCGamingModApp.Core/Helpers/ConversionHelper.cs
@@ -4,14 +4,16 @@ namespace PCGamingModApp.Core.Helpers;
 
 public static class ConversionHelper
 {
-    public static double ConvertBytesToUnit(long bytes, SizeUnit unit)
+    public static long ConvertBytesToUnit(long bytes, SizeUnit unit)
     {
         return unit switch
         {
-            SizeUnit.KB => bytes / 1024d,
-            SizeUnit.MB => bytes / (1024d * 1024d),
-            SizeUnit.GB => bytes / (1024d * 1024d * 1024d),
-            SizeUnit.TB => bytes / (1024d * 1024d * 1024d * 1024d),
+            SizeUnit.KB => bytes / 1024L,
+            SizeUnit.MB => bytes / (1024L * 1024L),
+            SizeUnit.GB => bytes / (1024L * 1024L * 1024L),
+            SizeUnit.TB => bytes / (1024L * 1024L * 1024L * 1024L),
+            SizeUnit.PB => bytes / (1024L * 1024L * 1024L * 1024L * 1024L),
+            SizeUnit.EB => bytes / (1024L * 1024L * 1024L * 1024L * 1024L * 1024L),
             _ => bytes
         };
     }
